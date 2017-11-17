2656ac94bb0a7dc216af3dfbfff99e36de34e7e4
anon2538 <bangrrom19@gmail.com>
2017-11-17 11:01:17 +0700

    Initial commit

---
+++ src/myFitstJAVA.java
@@ -0,0 +1,5 @@
+public class myFitstJAVA {
+    public static void main(String[] args) {
+        System.out.printIn("Hello Wordl, T'm non");
+    }//main
+}//class
---
+++ src/oop_lab/359211110129/rmutsv/com/myFitstJAVA.kt
@@ -0,0 +1,6 @@
+package oop_lab.`359211110129`.rmutsv.com;
+
+public class myFirstJAVA{
+    pa
+}//class
+
