915c2426daa59b7f0cf18a192ddff017813b9a00
LAB_COM-MT_1 <bangrrom19@gmail.com>
2017-12-29 10:33:57 +0700

    myFirstJAVA

--- src/OOP_labs359211110129/rmutsv/com/myFirstJAVA.java
+++ src/OOP_labs359211110129/rmutsv/com/myFirstJAVA.java
@@ -1,8 +1,8 @@
-package oop_lab.s359211110131.rmutsv.com;
+package ooplab.s359211110129.rmutsv.com;
 
 public class myFirstJAVA {
     public static void main(String[] args) {
-        System.out.println("Hello Word, I'm Wiparat");
+        System.out.println("Hello Word, I'm anon");
         System.out.println("Information System");
     }
 
